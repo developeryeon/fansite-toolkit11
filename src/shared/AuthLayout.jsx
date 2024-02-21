1aadf25f4a036383fb7e7886eb8fb4c7a5f00200
yeon <test2@test2.com>
2024-02-21 15:16:53 +0900

    [authLayout] 수정

--- src/shared/AuthLayout.jsx
+++ src/shared/AuthLayout.jsx
@@ -6,12 +6,6 @@ export default function AuthLayout() {
 	const auth = useSelector((state) => state.AuthSlice.users);
 	console.log('auth/token test : ', auth);
 
-	// 사용자 인증 여부
-	const isAuthenticated = () => {
-		const token = localStorage.getItem('accessToken');
-		return !token;
-	};
-
 	if (!auth) {
 		return <Navigate to={'/signup'} />;
 	}
@@ -19,7 +13,6 @@ export default function AuthLayout() {
 	return (
 		<>
 			<Outlet />
-			{/* return isAuthenticated() ? <Outlet /> : <Navigate to="/signup" />; */}
 		</>
 	);
 }
